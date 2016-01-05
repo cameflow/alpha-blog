ab2491276f15b9bdf0b3bd65e6b6bef63db86ee1
Alejandro <alex.terrazas.z@gmail.com>
2016-01-05 15:48:46 -0600

    :rocket: Create new template for new article

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+  # root 'welcome#index'
 
 
-   get 'pages/home', to: 'pages#home'
-   get 'pages/about', to: 'pages#about'
+   root 'pages#home'
+   get 'about', to: 'pages#about'
+
+   resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
